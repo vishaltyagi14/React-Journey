9df73d840b2037783436128fd0a2a7f89bc77c1f
Vishal Tyagi <vishal.tyagi14oct@gmail.com>
2026-06-08 12:49:43 +0530

    Todo app

---
+++ projects/TODO-Using-React/src/components/TodoForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState ,useContext} from 'react'
+import { OnlyContext } from '../Context/ContextApi'
+
+function TodoForm() {
+    const [title, setTitle] = useState("")
+   const {addTODO} =useContext(OnlyContext)
+
+    const add =(e)=>{
+        e.preventDefault();
+        if(!title) return
+        addTODO({title,completed:false})
+        setTitle("")
+    }
+
+    return (
+        <form  onSubmit={add} className="flex">
+            <input
+                type="text"
+                placeholder="Write Todo..."
+                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                value={title}
+                onChange={(e)=>setTitle(e.target.value)}
+            />
+            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+                Add
+            </button>
+        </form>
+    );
+}
+
+export default TodoForm;
+
